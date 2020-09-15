aca2a232c29ab65ab4e278165a3ae222bf7e6fc5
Et3rnauta <guidocanevello@hotmail.com>
2020-09-15 17:10:02 -0300

    Iniciar Frontend

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+//Init Server
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+//Init DB
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established succesfully");
+})
+
+//Start Server
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+})
