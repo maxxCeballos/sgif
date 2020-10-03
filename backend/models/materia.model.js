dd767c837e47be35e4e02d28590b6af649884ed8
yophex <seba_yaupe@hotmail.com>
2020-10-03 19:01:33 -0300

    Persona, Materia, Localidad Schemas actualizados

--- backend/models/materia.model.js
+++ backend/models/materia.model.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const materiaEsquema = new Schema({
-    
+    nombre: String,
+    anio: { type: Number, min: 1, max: 5 }
+
 }, { timestamps: true });
 
 const Materia = mongoose.model('Materia', materiaEsquema);
