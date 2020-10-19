d2665587df206165412084d0909b05df2654e249
GastonVidart <gaston.vidart@gmail.com>
2020-10-19 20:59:53 -0300

    Update Esquema Ciclo Lectivo y Curso Agregado uniques Posterior: ver creacion de singleton en Ciclo Lectivo

--- backend/models/cicloLectivo.model.js
+++ backend/models/cicloLectivo.model.js
@@ -3,11 +3,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const cicloLectivoEsquema = new Schema({
-    cicloLectivo: { type: Number, min: 1960, max: 9999 }, //FIXME ver el max que sea para el año actual
+    //FIXME ver el max que sea para el año actual    
+    cicloLectivo: { type: Number, min: 1960, max: 9999, unique: true },
     fechaIniClases: Date,
     fechaCiere1T: Date,
     fechaCiere2T: Date,
-    fechaCiere3T: Date    
+    fechaCiere3T: Date
 }, { timestamps: true });
 
 const CicloLectivo = mongoose.model('CicloLectivo', cicloLectivoEsquema);
--- backend/models/curso.model.js
+++ backend/models/curso.model.js
@@ -6,9 +6,12 @@ const cursoEsquema = new Schema({
     anio: { type: Number, min: 1, max: 5 },
     division: { type: Number, min: 1, max: 3 },
     cicloLectivo: Number, //Se copia desde el esquema de CicloLectivo
-    dictados = [Schema.Types.ObjectId],
-    alumnos = [Schema.Types.ObjectId],
-    preceptor = Schema.Types.ObjectId
+    dictados = [{type: Schema.Types.ObjectId, ref: 'Dictado'}],
+    alumnos = [{type: Schema.Types.ObjectId, ref: 'Alumno'}],
+    
+    //TODO: ATENCION! llenar solo con preceptor
+    // es Persona el esquema al que va a relacionar
+    preceptor = {type: Schema.Types.ObjectId, ref: 'Persona'}
 }, { timestamps: true});
 
 const Curso = mongoose.model('Curso', cursoEsquema);
