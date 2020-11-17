d0ad12986dfdaeab6ce3684a68a76172e5004690
GastonVidart <gaston.vidart@gmail.com>
2020-11-17 16:11:51 -0300

    Agregar Middleware validaReqInscAl

---
+++ backend/middlewares/validaReqInscAl.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const vValidarFecha = (req, res, next) => {
+
+}
+
+const vValidarAlumno = (req, res, next) => {
+
+}
+
+const vGetPersona = (req, res, next) => {
+    //TODO: reciclar
+}
+
+const vGetResponsable = (req, res, next) => {
+
+}
+
+const vResponsableNuevo = (req, res, next) => {
+
+}
+
+const vResponsableRol = (req, res, next) => {
+
+}
+
+const vRegistrarAlumno = (req, res, next) => {
+
+}
+
+const vReinscribirAlumno = (req, res, next) => {
+
+}
+
+module.exports = {
+    vValidarFecha,
+    vValidarAlumno,
+    vGetPersona,
+    vGetResponsable,
+    vResponsableNuevo,
+    vResponsableRol,
+    vRegistrarAlumno,
+    vReinscribirAlumno
+}
