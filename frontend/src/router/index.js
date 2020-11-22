c61d057dfe742a4b7ad09f7eb6b9032363e9a25a
Guido Canevello <guidocanevello@hotmail.com>
2020-11-22 17:19:55 -0300

    Implementacion Inscribir Mesa Front

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -17,6 +17,13 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },{
+    path: '/inscribir-mesa',
+    name: 'Inscribir en Mesa de Examen',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/transacciones/inscribir-mesa/InscribirMesa.vue')
   }
 ]
 
---
+++ frontend/src/views/transacciones/inscribir-mesa/InscribirMesa.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <div>
+      <md-table>
+        <md-table-row>
+          <md-table-head>Nombre</md-table-head>
+          <md-table-head md-numeric>Año</md-table-head>
+          <md-table-head>Ciclo Lectivo</md-table-head>
+        </md-table-row>
+
+        <!-- <div v-bind:key="materia.id" v-for="materia in materias"> -->
+          <md-table-row v-bind:key="materia.id" v-for="materia in materias">
+            <md-table-cell> {{materia.nombre}} </md-table-cell>
+            <md-table-cell md-numeric> {{materia.anio}} </md-table-cell>
+            <md-table-cell> {{materia.cicloLectivo}} </md-table-cell>
+          </md-table-row>
+        <!-- </div> -->
+      </md-table>
+    </div>
+
+    <div v-bind:key="materia.id" v-for="materia in materias">
+      {{ materia.nombre }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InscribirMesa",
+  component: {},
+  data() {
+    return {
+      materias: [
+        {
+          id: 1,
+          nombre: "Matematicas",
+          anio: 1,
+          cicloLectivo: 2018,
+        },
+        {
+          id: 2,
+          nombre: "Lengua",
+          anio: 3,
+          cicloLectivo: 2020,
+        },
+        {
+          id: 3,
+          nombre: "Biologia",
+          anio: 2,
+          cicloLectivo: 2019,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
