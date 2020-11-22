41e60a500c5917fd1d46dea8254cad18441caa80
Guido Canevello <guidocanevello@hotmail.com>
2020-11-22 18:13:03 -0300

    Instanciado Test Inscribir Mesa

---
+++ test/backend/inscribir-mesa/testInscribirMesa.js
@@ -0,0 +1,15 @@
+const assert = require("chai").assert;
+const { Assertion } = require("chai");
+const utlBackend = "localhost:3000"
+
+
+describe('InscribirMesa', () => {
+    describe('sayHello()', function () {
+        it('app should return hello', function () {
+            // let result = app.sayHello();
+            assert.equal(1, 1);
+        });
+    })
+})
+
+const 
