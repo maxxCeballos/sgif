a0cd809864c479e10082681d1b15f03fe6d8b452
yophex <seba_yaupe@hotmail.com>
2020-11-26 16:50:47 -0300

    ayuda de guido

---
+++ test/transacciones/testAgregarDatosMesa.js
@@ -0,0 +1,17 @@
+const assert = require('chai').assert;
+
+
+
+
+describe('Agregar Datos Mesa de Examen', function () {
+
+  it('deberia devolver una lista de mesas en estado solicitada', function () {
+    assert.equal(true, true);
+  });
+
+
+  it('deberia devolver una lista vacia', function () {
+    //el segundo argumento es lo que deberia dar
+    assert.equal(true, true);
+  });
+});
