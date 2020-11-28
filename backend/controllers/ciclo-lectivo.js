de1d01397fae0282e38d278e819526340f7bf40b
GastonVidart <gaston.vidart@gmail.com>
2020-11-28 15:19:29 -0300

    Fix menores por testing

--- backend/controllers/ciclo-lectivo.js
+++ backend/controllers/ciclo-lectivo.js
@@ -5,11 +5,10 @@ const { trace } = require('../routes/ciclo-lectivo');
 
 const getCicloLectivo = async () => {
     const cicloLectivoDB = await CicloLectivo.find().sort({ cicloLectivo: -1 }).exec();
-    //TODO: CUIDADO hay un ciclo lectivo del 2021
-    return cicloLectivoDB[1];
+    return cicloLectivoDB[0];
 }
 
-const getCicloLectivoById = async (cicloLectivo) =>{
+const getCicloLectivoById = async (cicloLectivo) => {
     const cicloLectivoDB = await CicloLectivo.find({ cicloLectivo: cicloLectivo }).exec();
     return cicloLectivoDB;
 }
@@ -28,4 +27,4 @@ const createCicloLectivo = async (cicloLectivoObj) => {
     return cicloLectivoDB;
 }
 
-module.exports = { getCicloLectivo, createCicloLectivo, getCicloLectivoById}
+module.exports = { getCicloLectivo, createCicloLectivo, getCicloLectivoById }
