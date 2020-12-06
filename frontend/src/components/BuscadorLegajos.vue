d58a0c21fc7686be06f18938dfacf67e835418e0
Guido Canevello <guidocanevello@hotmail.com>
2020-12-06 20:41:24 -0300

    Mejorado el sistema de buscado para no actualizar la pagina

--- frontend/src/components/BuscadorLegajos.vue
+++ frontend/src/components/BuscadorLegajos.vue
@@ -3,13 +3,13 @@
     <v-card elevation="2" outlined>
       <v-card-title>Ingrese un Legajo</v-card-title>
       <v-card-text>
-        <v-form @submit="$emit('set-legajo', legajo)">
+        <v-form @submit="setLegajo">
           <v-row>
             <v-col>
               <v-text-field v-model="legajo" label="Ingrese un Legajo" />
             </v-col>
             <v-col>
-              <v-btn @click="$emit('set-legajo', legajo)" elevation="1" large
+              <v-btn @click="setLegajo" elevation="1" large
                 >Buscar Materias
               </v-btn>
             </v-col>
@@ -27,6 +27,13 @@ export default {
     return {
       legajo: "",
     };
+  },
+
+  methods: {
+    setLegajo(e) {
+      e.preventDefault();
+      this.$emit("set-legajo", this.legajo);
+    },
   },
 };
 </script>
