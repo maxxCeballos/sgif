6c44cdb419ecaf798ef5507db0fbb5eb4ea35ded
Guido Canevello <guidocanevello@hotmail.com>
2020-12-10 18:28:39 -0300

    Implementado API de versiculos

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -5,8 +5,17 @@
         <v-card-title primary-title> MENSAJE DE BIENVENIDA </v-card-title>
         <v-card-text> Hola, Bienvenido </v-card-text>
       </v-card>
-      
     </v-container>
+    
+    <carta-citas-biblia />
   </div>
 </template>
+
+<script>
+import CartaCitasBiblia from "../components/home/CartaCitasBiblia.vue";
+export default {
+  name: "Home",
+  components: { CartaCitasBiblia },
+};
+</script>
 
